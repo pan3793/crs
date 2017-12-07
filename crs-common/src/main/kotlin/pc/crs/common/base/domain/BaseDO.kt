25285b4e859f23b1116167d78621844be15e4999
pancheng <pancheng@juxiangfen.com>
2017-12-07 18:32:31 +0800

    优化基类

--- crs-common/src/main/kotlin/pc/crs/common/base/domain/BaseDO.kt
+++ crs-common/src/main/kotlin/pc/crs/common/base/domain/BaseDO.kt
@@ -1,6 +1,8 @@
 package pc.crs.common.base.domain
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import java.io.Serializable
 import java.time.LocalDateTime
 import javax.persistence.*
@@ -9,7 +11,7 @@ import javax.persistence.*
 abstract class BaseDO(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long = 0L,
         @Column(nullable = false) var note: String = "",
-        @JsonIgnore @Column(nullable = false) var createTime: LocalDateTime = LocalDateTime.now(),
-        @JsonIgnore @Column(nullable = false) var modifiedTime: LocalDateTime = LocalDateTime.now(),
+        @JsonIgnore @CreatedDate @Column(nullable = false) var createTime: LocalDateTime = LocalDateTime.now(),
+        @JsonIgnore @LastModifiedDate @Column(nullable = false) var modifiedTime: LocalDateTime = LocalDateTime.now(),
         @JsonIgnore @Column(nullable = false) var enabled: Boolean = true
 ) : Serializable
--- crs-common/src/main/kotlin/pc/crs/common/base/service/BaseService.kt
+++ crs-common/src/main/kotlin/pc/crs/common/base/service/BaseService.kt
@@ -1,4 +1,28 @@
 package pc.crs.common.base.service
 
-class BaseService {
+import org.springframework.data.jpa.repository.JpaRepository
+
+abstract class BaseService<DO, DAO : JpaRepository<DO, Long>>(val dao: DAO) {
+
+    fun save(bean: DO) {
+        dao.save(bean)
+    }
+
+    fun remove(id: Long) {
+        dao.deleteById(id)
+    }
+
+    fun remove(bean: DO) {
+        dao.delete(bean)
+    }
+
+    fun findOne(id: Long): DO? {
+        dao.findOne(id)
+    }
+
+    fun findFirst(bean: DO): DO? {
+
+    }
+
+    fun findAll()
 }
