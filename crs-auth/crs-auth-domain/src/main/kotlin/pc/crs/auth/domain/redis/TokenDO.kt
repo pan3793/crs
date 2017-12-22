25813b8b215c3e93c8c0411d0b14f8873f16ac17
pancheng <pancheng@juxiangfen.com>
2017-12-22 13:36:18 +0800

    修复gradle插件和依赖配置导致的编译车祸；

--- crs-auth/crs-auth-domain/src/main/kotlin/pc/crs/auth/domain/redis/TokenDO.kt
+++ crs-auth/crs-auth-domain/src/main/kotlin/pc/crs/auth/domain/redis/TokenDO.kt
@@ -9,6 +9,4 @@ import javax.persistence.Id
 @RedisHash(value = "crs:auth:token", timeToLive = 30 * 60)
 data class TokenDO(@Id val id: String = UUID.randomUUID().toString(),
                    @Indexed val clientId: Long,
-                   @Indexed val userId: Long) : Serializable {
-    constructor() : this(clientId = -1, userId = -1)
-}
+                   @Indexed val userId: Long) : Serializable
