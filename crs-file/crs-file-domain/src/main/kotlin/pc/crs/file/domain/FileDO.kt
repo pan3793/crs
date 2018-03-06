237de5706bb9fce725212e46c207a71d1f62f42e
pancheng <379377944@qq.com>
2018-03-06 20:06:53 +0800

    fix FileDO

--- crs-file/crs-file-domain/src/main/kotlin/pc/crs/file/domain/FileDO.kt
+++ crs-file/crs-file-domain/src/main/kotlin/pc/crs/file/domain/FileDO.kt
@@ -2,9 +2,10 @@ package pc.crs.file.domain
 
 import pc.crs.common.base.domain.BaseDO
 import javax.persistence.Column
+import javax.persistence.Entity
 import javax.persistence.Table
 
-
+@Entity
 @Table(name = "file")
 data class FileDO(
         @Column(nullable = false) var name: String = "",
