39163e1ee38010097d0661f0df085e95e68471e1
pancheng <pancheng@juxiangfen.com>
2018-03-07 11:45:02 +0800

    fix CardDO

--- crs-domain/src/main/kotlin/pc/crs/domain/CardDO.kt
+++ crs-domain/src/main/kotlin/pc/crs/domain/CardDO.kt
@@ -4,8 +4,10 @@ import pc.crs.common.base.domain.BaseDO
 import pc.crs.common.convert.LongListJsonConverter
 import javax.persistence.Column
 import javax.persistence.Convert
+import javax.persistence.Entity
 import javax.persistence.Table
 
+@Entity
 @Table(name = "card")
 data class CardDO(
         @Column(nullable = false) var title: String = "",
