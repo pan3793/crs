84f4ae17f028bafaf7e4f78150890298bb3514f2
pancheng <pancheng@juxiangfen.com>
2018-03-23 14:12:29 +0800

    fix BaseExceptionHandler package name

--- crs-common/src/main/kotlin/pc/crs/common/base/handle/BaseExceptionHandler.kt
+++ crs-common/src/main/kotlin/pc/crs/common/base/handle/BaseExceptionHandler.kt
@@ -1,4 +1,4 @@
-package pc.crs.common.config
+package pc.crs.common.base.handle
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
--- crs-forum/crs-forum-server/src/main/kotlin/pc/crs/forum/server/config/GlobalExceptionHandler.kt
+++ crs-forum/crs-forum-server/src/main/kotlin/pc/crs/forum/server/config/GlobalExceptionHandler.kt
@@ -2,7 +2,7 @@ package pc.crs.forum.server.config
 
 import org.springframework.core.annotation.Order
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import pc.crs.common.config.BaseExceptionHandler
+import pc.crs.common.base.handle.BaseExceptionHandler
 
 @Order(1)
 @RestControllerAdvice
