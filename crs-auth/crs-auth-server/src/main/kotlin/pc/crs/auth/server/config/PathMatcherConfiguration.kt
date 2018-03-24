ee388f25c11e79128380e293a00516e20cbdb4a2
pancheng <379377944@qq.com>
2018-03-24 15:19:28 +0800

    Feign stater 依赖存在问题，回退F.M8

--- crs-auth/crs-auth-server/src/main/kotlin/pc/crs/auth/server/config/PathMatcherConfiguration.kt
+++ crs-auth/crs-auth-server/src/main/kotlin/pc/crs/auth/server/config/PathMatcherConfiguration.kt
@@ -1,4 +1,4 @@
-package pc.crs.auth.server.config;
+package pc.crs.auth.server.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
--- crs-file/crs-file-server/src/main/kotlin/pc/crs/file/server/config/FeignConfiguration.kt
+++ crs-file/crs-file-server/src/main/kotlin/pc/crs/file/server/config/FeignConfiguration.kt
@@ -1,9 +1,11 @@
 package pc.crs.file.server.config
 
 import feign.codec.Decoder
+import feign.optionals.OptionalDecoder
 import org.springframework.beans.factory.ObjectFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
+import org.springframework.cloud.openfeign.support.SpringDecoder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import pc.crs.common.config.CustomizedResponseEntityDecoder
