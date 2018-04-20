5a962cb75e40b8d84cf827b82bb3bb07e3dbe600
pancheng <379377944@qq.com>
2018-04-20 12:12:16 +0800

    删除classwork、classwork_task、exam_task

--- crs-server/src/main/kotlin/pc/crs/server/dao/ClassworkDAO.kt
+++
@@ -1,8 +0,0 @@
-package pc.crs.server.dao
-
-import org.springframework.stereotype.Repository
-import pc.crs.common.base.dao.BaseDAO
-import pc.crs.domain.ClassworkDO
-
-@Repository
-interface ClassworkDAO : BaseDAO<ClassworkDO>
--- crs-server/src/main/kotlin/pc/crs/server/dao/ClassworkTaskDAO.kt
+++
@@ -1,8 +0,0 @@
-package pc.crs.server.dao
-
-import org.springframework.stereotype.Repository
-import pc.crs.common.base.dao.BaseDAO
-import pc.crs.domain.ClassworkTaskDO
-
-@Repository
-interface ClassworkTaskDAO : BaseDAO<ClassworkTaskDO>
