185f88d05e9d92ad2f1c70fad3a64065568a5d22
pancheng <379377944@qq.com>
2018-05-11 13:13:48 +0800

    调整JsonUtil，打印空字段

--- crs-common/src/main/java/pc/crs/common/util/JsonUtil.java
+++ crs-common/src/main/java/pc/crs/common/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package pc.crs.common.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 /**
  * @author pancheng
@@ -8,14 +9,16 @@ import com.alibaba.fastjson.JSON;
 public class JsonUtil {
 
     public static String toJsonString(Object object) {
-        return JSON.toJSONString(object);
+        return JSON.toJSONString(object, SerializerFeature.WriteMapNullValue);
     }
 
     public static String toJsonString(Object object, boolean prettyFormat) {
-        return JSON.toJSONString(object, prettyFormat);
+        if (prettyFormat)
+            return JSON.toJSONString(object, SerializerFeature.WriteMapNullValue, SerializerFeature.PrettyFormat);
+        return JSON.toJSONString(object, SerializerFeature.WriteMapNullValue);
     }
 
     public static String formatJson(String origin) {
-        return JSON.toJSONString(JSON.parse(origin), true);
+        return JSON.toJSONString(JSON.parse(origin), SerializerFeature.WriteMapNullValue, SerializerFeature.PrettyFormat);
     }
 }
